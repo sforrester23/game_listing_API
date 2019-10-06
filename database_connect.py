32142950e0d5b6edf932454efa2501317f266edb
Sam Forrester <SForrester@spartaglobal.com>
2019-10-06 14:40:23 +0100

    added methods for creating data in the table, reading all entries, reading some entries based on values (e.g. ID, game name, price range) and updating values

--- database_connect.py
+++ database_connect.py
@@ -15,3 +15,39 @@ class db_connect():
     def __filter_query(self, query):
         return self.cursor.execute(query)
 
+    # define a set of methods to CRUD our data in the database
+    # C - create
+    def create_entry(self, name, phone_number, price, location):
+        self.__filter_query("INSERT INTO game_info (game_name, contact_number, price, location) VALUES ('{}', '{}', {}, '{}'".format(name, phone_number, price, location))
+        self.connect_db.commit()
+    # R - read (based on either game_listing, game_name, price range)
+    def print_all_listings(self):
+        all_game_query = self.__filter_query("SELECT * FROM game_info")
+        while True:
+            record = all_game_query.fetchone()
+            if record is None:
+                break
+            print(record)
+
+    def read_entry_game_id(self, listing_id):
+        self.__filter_query("SELECT * FROM game_info WHERE listing_ID = {}".format(listing_id))
+
+    def read_entry_game_name(self, game_name):
+        self.__filter_query("SELECT * FROM game_info WHERE game_name = '{}'".format(game_name))
+
+    def read_entry_price_range(self, lower_bound, upper_bound):
+        self.__filter_query("SELECT * FROM game_info WHERE price > {} AND price < {}".format(lower_bound, upper_bound))
+
+    # U - update
+    def update_entry(self, listing_id, column_to_update, new_value):
+        # if its price you'd like to update, we need to change the query so it doesn't make an integer a string
+        if column_to_update == 'price':
+            self.__filter_query("UPDATE game_info SET {} = {} WHERE listing_ID = {}".format(column_to_update, new_value, listing_id))
+            self.connect_db.commit()
+        # if its not price, we need to make sure the query takes an integer to input as the new value
+        else:
+            self.__filter_query("UPDATE game_info SET {} = '{}' WHERE listing_ID = {}".format(column_to_update, new_value, listing_id))
+            self.connect_db.commit()
+
+    
+
